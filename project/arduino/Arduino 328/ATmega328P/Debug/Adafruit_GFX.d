9997ec7d1daf1b2eafd8997c5f56593805679c5b
warrior-rockk <warrior-rockk@bdfe1ff2-85b1-43ac-a073-3d879cfd3ae1>
2017-07-31 16:23:46 +0000

    añadimos control lcd y encoder rotativo
    git-svn-id: https://svn.code.sf.net/p/warcom/code/trunk/BeatDuino@1016 bdfe1ff2-85b1-43ac-a073-3d879cfd3ae1

---
+++ project/arduino/Arduino 328/ATmega328P/Debug/Adafruit_GFX.d
@@ -0,0 +1,96 @@
+Adafruit_GFX.o: ../Adafruit_GFX.cpp ../Adafruit_GFX.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Arduino.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdlib.h \
+  c:\archivos\ de\ programa\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stddef.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/string.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/math.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/inttypes.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdint.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/io.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/iom328p.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/common.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/version.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/lock.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/interrupt.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/binary.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/WCharacter.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/ctype.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/WString.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Stream.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Print.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdio.h \
+  c:\archivos\ de\ programa\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stdarg.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Printable.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/new.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/variants/standard/pins_arduino.h \
+  C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Print.h \
+  ../glcdfont.c
+
+../Adafruit_GFX.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Arduino.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdlib.h:
+
+c:\archivos\ de\ programa\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/string.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/math.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/inttypes.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdint.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/io.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/iom328p.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/common.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/version.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/interrupt.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/binary.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/WCharacter.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/ctype.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/WString.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/HardwareSerial.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Stream.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Print.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdio.h:
+
+c:\archivos\ de\ programa\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stdarg.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Printable.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/new.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/variants/standard/pins_arduino.h:
+
+C:/Archivos\ de\ programa/Arduino/hardware/arduino/cores/arduino/Print.h:
+
+../glcdfont.c:
---
+++ project/arduino/Arduino 328/ATmega328P/Debug/glcdfont.d
@@ -0,0 +1,37 @@
+glcdfont.o: ../glcdfont.c \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/io.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/inttypes.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdint.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/iom328p.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/common.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/version.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/lock.h \
+  c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\archivos\ de\ programa\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stddef.h
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/io.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/inttypes.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdint.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/iom328p.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/common.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/version.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/archivos\ de\ programa/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\archivos\ de\ programa\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
