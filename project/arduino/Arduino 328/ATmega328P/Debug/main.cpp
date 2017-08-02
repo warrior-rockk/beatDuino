f7f0febfc699fae1865c1d76a5a96ed4c704d8ff
warrior-rockk <warrior-rockk@bdfe1ff2-85b1-43ac-a073-3d879cfd3ae1>
2017-08-02 16:29:41 +0000

    organizamos la secuencia de trabajo y empezamos con estructura de memoria de los repetorios
    git-svn-id: https://svn.code.sf.net/p/warcom/code/trunk/BeatDuino@1023 bdfe1ff2-85b1-43ac-a073-3d879cfd3ae1

--- project/arduino/Arduino 328/ATmega328P/Debug/main.cpp
+++ project/arduino/Arduino 328/ATmega328P/Debug/main.cpp
@@ -13,7 +13,8 @@
  
  */
 
- #include <avr/wdt.h> 
+#include <avr/wdt.h> 
+#include <EEPROM.h>
 //comunicacion con LCD OLED
 #include <SPI.h>
 #include <Wire.h>
@@ -25,10 +26,12 @@
 #include <images.h>
 void setup ();
 void loop ();
+void playMetronome ();
+void stopMetronome ();
 void wellcomeTest ();
 void doEncoder ();
 void processButton (int pin ,int buttonNum );
-#line 23
+#line 24
 
 //Definiciones====================================
 
@@ -44,6 +47,9 @@ void processButton (int pin ,int buttonNum );
 #define OLED_RESET 	4
 #define START_STOP  0
 
+//botones
+#define START_STOP_BT 0
+
 //config LCD
 Adafruit_SSD1306 display(OLED_RESET);
 #if (SSD1306_LCDHEIGHT != 64)
@@ -55,12 +61,21 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define EIGHTH		2
 #define SIXTEENTH   4
 
+//modos
+#define METRONOME_MODE   0
+#define LIVE_MODE        1
+
+//repertorios y canciones
+#define MAX_PLAYLISTS	5
+#define MAX_SONGS       30
+#define MAX_TITLE       15
+
 //==============================================
 
 //Estructuras===================================
 
 //Estructura estado botones
-struct buttons
+struct button_
 {
    bool pressed;
    bool longPress;
@@ -70,30 +85,48 @@ struct buttons
    unsigned int timerOff;
    unsigned int timerLong;
 }button[2];
+
+//tipo de estructura de una cancion
+typedef struct
+{
+	String title;
+	unsigned int tempo;
+	unsigned int noteDivision;
+	unsigned int beatSignature;
+}song_;
+
+//estructura de repetorio
+struct playList_
+{
+	String title;
+	song_ song[MAX_SONGS];
+}playList;
+
 //==============================================
 
 //Variables generales===========================
-
-unsigned int bpm 			= 100;		//tempo general
-unsigned long msTempo 		= 0;		//tempo en milisegundos
-unsigned int clickDuration 	= 10;		//duración pulso click
-unsigned long lastTime 		= 0;		//memoria tiempo anterior
-unsigned int noteDivision	= QUARTER;//subdivision nota click
-unsigned int barSignature   = 4;		//tipo compas
-unsigned int actualTick     = 1;		//tiempo actual
-boolean tick 				= true;		//flag de activar tick
-boolean play				= false;	//flag de activar metronomo
+unsigned int mode           = METRONOME_MODE;	//modo general
+boolean refreshLCD			= false;			//refresco LCD
+unsigned int bpm 			= 100;				//tempo general
+unsigned long msTempo 		= 0;				//tempo en milisegundos
+unsigned int clickDuration 	= 10;				//duración pulso click
+unsigned long lastTime 		= 0;				//memoria tiempo anterior
+unsigned int noteDivision	= QUARTER;			//subdivision nota click
+unsigned int barSignature   = 4;				//tipo compas
+unsigned int actualTick     = 1;				//tiempo actual
+boolean tick 				= true;				//flag de activar tick
+boolean play				= false;			//flag de activar metronomo
 //interfaz
-signed int deltaEnc             = 0;		//incremento o decremento del encoder
-unsigned int buttonDelay     	= 2;		//Tiempo antirebote
-unsigned int buttonLongPress 	= 60;		//Tiempo pulsacion larga para otras funciones
+signed int deltaEnc             = 0;			//incremento o decremento del encoder
+unsigned int buttonDelay     	= 2;			//Tiempo antirebote
+unsigned int buttonLongPress 	= 60;			//Tiempo pulsacion larga para otras funciones
 
 //configuracion
 void setup()
  { 
 	//desactivamos el watchdog
 	wdt_disable();
-		
+	
 	//configuramos los pines
 	pinMode(ENC_A,INPUT_PULLUP);
 	pinMode(ENC_B,INPUT_PULLUP);
@@ -121,84 +154,121 @@ void setup()
 	
 	//activamos el watchdog a 2 segundos
 	wdt_enable(WDTO_2S);
+	
+	/*playList[0].title = "Largo";
+	playList[0].song[0].title = "De que se rie";
+	playList[0].song[0].tempo = 168;
+	playList[0].song[0].noteDivision = QUARTER;
+	playList[0].song[0].beatSignature = 4;*/
  }
 
  //bucle principal
 void loop()
  { 
-	//conversion bpm
-	bpm = bpm + deltaEnc;
-	deltaEnc = 0;
-	
-	//ms of actual tempo
-	msTempo = (60000/bpm);
-	
-	//procesamos botones
-	processButton(START_STOP,0);
-	
-	//arranque-paro
-	if (button[0].pEdgePress)
-		play = !play;
-		
-	//si está activado el metronomo
-	if (play)
+	//comprobamos modo
+	switch (mode)
 	{
-		//comprobamos si se cumple el siguiente tick si el tiempo supera el tiempo de negra dividido entre
-		//la division de notas seleccionada
-		if ((millis()-lastTime) >= (msTempo/noteDivision))
-		{
-			//realizamos tick
-			tick = true;
-			//incrementamos el numero de tick del compas
-			actualTick == (barSignature*noteDivision) ? actualTick = 1 : actualTick++;
-			lastTime = millis();		 
-		}
-		
-		//actualizamos tick
-		if (tick){
-			//led
-			digitalWrite(LED_CLICK, HIGH);
-			//sonido del tick según si es el primer tiempo del compás
-			if (actualTick == 1)
-				tone(OUT_CLICK,NOTE_A4,clickDuration);
+		//modo metronomo normal. Con la ruleta cambias el tempo.
+		case METRONOME_MODE:
+			//conversion bpm
+			bpm = bpm + deltaEnc;
+			deltaEnc = 0;
+			
+			//ms of actual tempo
+			msTempo = (60000/bpm);
+			
+			//procesamos botones
+			processButton(START_STOP,START_STOP_BT);
+			
+			//arranque-paro del sonido
+			if (button[START_STOP_BT].pEdgePress)
+				play = !play;
+				
+			//si está activado el sonido del metronomo
+			if (play)
+			{
+				playMetronome();
+			}
 			else
-				tone(OUT_CLICK,NOTE_C4,clickDuration);
-			//desactivamos flag	
-			tick = false;
-		}
-		else{
-			digitalWrite(LED_CLICK, LOW);
-			noTone(OUT_CLICK);
-		}		
-	}
-	else
-	{
-		digitalWrite(LED_CLICK, LOW);
-		noTone(OUT_CLICK);
-		tick = false;
-		actualTick = 0;
-		lastTime = millis();
+			{
+				stopMetronome();
+			}
+			
+			refreshLCD = true;
+			break;
 	}
 	
-	//actualizamos display
-	display.clearDisplay();
-	display.setCursor(0,0);
-	display.print("01.Revolviendo\n\n"); 
-	display.setTextSize(2);
-	display.print(bpm); 
-	display.print(" BPM\n\n"); 
-	display.setTextSize(1);
-	display.print(barSignature);
-	display.print("/");
-	display.print(noteDivision*4);
-	display.print("      ");
-	play ? display.print("START") : display.print("STOP");
-	display.display();
+	
+	//refresco LCD
+	if (refreshLCD) {
+		switch(mode)
+		{
+			case METRONOME_MODE:
+				//actualizamos display del modo metronomo
+				display.clearDisplay();
+				display.setCursor(0,0);
+				display.print("01.Revolviendo\n\n"); 
+				display.setTextSize(2);
+				display.print(bpm); 
+				display.print(" BPM\n\n"); 
+				display.setTextSize(1);
+				display.print(barSignature);
+				display.print("/");
+				display.print(noteDivision*4);
+				display.print("      ");
+				play ? display.print("START") : display.print("STOP");
+				display.display();
+				
+				break;
+		}
+		refreshLCD = false;
+	}
 	
 	//reseteamos el watchdog
 	wdt_reset();
  }
- 
+
+ //funcion que reproduce el metronomo al tempo y tipo de ritmo seleccionado
+ void playMetronome()
+ {
+	//comprobamos si se cumple el siguiente tick si el tiempo supera el tiempo de negra dividido entre
+	//la division de notas seleccionada
+	if ((millis()-lastTime) >= (msTempo/noteDivision))
+	{
+		//realizamos tick
+		tick = true;
+		//incrementamos el numero de tick del compas
+		actualTick == (barSignature*noteDivision) ? actualTick = 1 : actualTick++;
+		lastTime = millis();		 
+	}
+	
+	//actualizamos tick
+	if (tick){
+		//led
+		digitalWrite(LED_CLICK, HIGH);
+		//sonido del tick según si es el primer tiempo del compás
+		if (actualTick == 1)
+			tone(OUT_CLICK,NOTE_A4,clickDuration);
+		else
+			tone(OUT_CLICK,NOTE_C4,clickDuration);
+		//desactivamos flag	
+		tick = false;
+	}
+	else{
+		digitalWrite(LED_CLICK, LOW);
+		noTone(OUT_CLICK);
+	}		
+}
+
+//funcion que detiene la reproduccion del metronomo
+void stopMetronome()
+{
+	digitalWrite(LED_CLICK, LOW);
+	noTone(OUT_CLICK);
+	tick = false;
+	actualTick = 0;
+	lastTime = millis();
+}
  //funcion que realiza mensaje de inicio y test luces
 void wellcomeTest()
 {
